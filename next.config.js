818026e0ddcc0ff8745eff24c7f49292150031ae
ahadjawaid <ahadjawaid@gmail.com>
2024-03-18 13:23:19 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// Default Pages configuration for Next
+const nextConfig = { 
+    images: { 
+        unoptimized: true 
+    }, 
+    experimental: { images: { unoptimized: true } }, 
+    output: 'export',
+    basePath: "/styleMoE" 
+}
+module.exports = nextConfig
